a327aaf351fe582abc75c100d67cd165df599690
Cody Tanner <ctanner@instructure.com>
2018-01-15 12:22:43 -0700

    Adds course affiliate account share/revoke
    Change-Id: If8bfe6d7ef4b0d2dc2009ba726b4a7ab504753d9

--- lib/bridge_api/client/sub_account.rb
+++ lib/bridge_api/client/sub_account.rb
@@ -4,6 +4,14 @@ module BridgeAPI
       def get_sub_accounts(params = {})
         get("#{API_PATH}#{ADMIN_PATH}#{SUB_ACCOUNT_PATH}", params)
       end
+
+      def share_course_with_subaccount(course_template_id, params = {})
+        put("#{API_PATH}#{AUTHOR_PATH}#{COURSE_TEMPLATE_PATH}/#{course_template_id}/#{SUB_ACCOUNT_PATH}/share", params)
+      end
+
+      def revoke_course_from_subaccount(course_template_id, params = {})
+        put("#{API_PATH}#{AUTHOR_PATH}#{COURSE_TEMPLATE_PATH}/#{course_template_id}/#{SUB_ACCOUNT_PATH}/revoke", params)
+      end
     end
   end
 end
--- lib/bridge_api/version.rb
+++ lib/bridge_api/version.rb
@@ -1,3 +1,3 @@
 module BridgeAPI
-  VERSION = '0.1.15' unless defined?(BridgeAPI::VERSION)
+  VERSION = '0.1.16' unless defined?(BridgeAPI::VERSION)
 end
